ddc5ccdfcc3e2f381106ac96135a2004bbabaf75
Dito Arya Saputra <dito.20038@mhs.unesa.ac.id>
2021-10-11 22:59:39 +0700

    Change-3: Penambahan layout untuk bagian materi di sertai fitur kembali ke halaman sebelumnya

--- app/src/main/java/com/example/promob/MenuActivity.java
+++ app/src/main/java/com/example/promob/MenuActivity.java
@@ -6,16 +6,28 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MenuActivity extends AppCompatActivity {
 
     private ImageView btnback;
+    private Button btnmtr1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        btnmtr1 = findViewById(R.id.btnmtr1);
+
+        btnmtr1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent GoTo = new Intent(MenuActivity.this,MtrActivity1.class);
+                startActivity(GoTo);
+            }
+        });
 
         btnback = findViewById(R.id.btnback);
 
---
+++ app/src/main/java/com/example/promob/MtrActivity1.java
@@ -0,0 +1,28 @@
+package com.example.promob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MtrActivity1 extends AppCompatActivity {
+
+    private ImageView btnback2mn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mtr1);
+
+        btnback2mn = findViewById(R.id.btnback2mn);
+
+        btnback2mn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MtrActivity1.this,MenuActivity.class));
+            }
+        });
+    }
+}
